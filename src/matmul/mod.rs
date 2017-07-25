91c03a0f2fa3b6aabad08a123a285d12446d1ced
Markus Baur <markusb@selfnet.de>
2017-07-25 19:28:03 +0200

    Add tiled matrix multiply and support for it

--- src/matmul/mod.rs
+++ src/matmul/mod.rs
@@ -9,8 +9,13 @@ pub mod naive_reordered;
 pub mod naive_simd;
 pub mod naive_rayon;
 
+pub mod blocked;
+
 pub mod matrix;
 pub use self::matrix::Matrix;
+
+pub mod tiledmatrix;
+pub use self::tiledmatrix::TileMatrix;
 
 #[cfg(test)]
 mod test {
@@ -28,24 +33,40 @@ mod test {
         let mut compare = Matrix::zero(n, n);
         naive::mult(&a, &b, &mut compare);
 
+        let a_t = TileMatrix::from(a.clone());
+        let b_t = TileMatrix::from(b.clone());
+        let compare_t = TileMatrix::from(compare.clone());
+
         let mut res = Matrix::zero(n, n);
+        let mut res_t = TileMatrix::from(res.clone());
+
         naive_unchecked::mult(&a, &b, &mut res);
         assert_eq!(compare, res);
+        res.reset();
 
         naive_reordered::mult(&a, &b, &mut res);
         assert_eq!(compare, res);
+        res.reset();
 
         naive_simd::mult(&a, &b, &mut res);
-        File::create("/tmp/a").unwrap().write_all(format!("{:?}", a).as_bytes());
-        File::create("/tmp/b").unwrap().write_all(format!("{:?}", b).as_bytes());
-
-        File::create("/tmp/simd").unwrap().write_all(format!("{:?}", res).as_bytes());
-        File::create("/tmp/comp").unwrap().write_all(format!("{:?}", compare).as_bytes());
-
         assert_eq!(compare, res);
+        res.reset();
 
         naive_rayon::mult(&a, &b, &mut res);
         assert_eq!(compare, res);
+        res.reset();
+
+        blocked::mult(&a_t, &b_t, &mut res_t);
+
+        File::create("/tmp/a").unwrap().write_all(format!("{}", a_t).as_bytes());
+        File::create("/tmp/b").unwrap().write_all(format!("{}", b_t).as_bytes());
+
+        File::create("/tmp/simd").unwrap().write_all(format!("{}", res_t).as_bytes());
+        File::create("/tmp/comp").unwrap().write_all(format!("{}", compare_t).as_bytes());
+        File::create("/tmp/comp_orig").unwrap().write_all(format!("{}", compare).as_bytes());
+
+        assert_eq!(compare_t, res_t);
+        res.reset();
     }
 }
 
